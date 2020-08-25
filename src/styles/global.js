346138a4bc3c3febb0f457a450374e43447b70a3
tonnyrogers <antoniel15975@gmail.com>
2020-08-25 19:12:04 -0300

    Config Directory / Styled Components

---
+++ src/styles/global.js
@@ -0,0 +1,36 @@
+ import { createGlobalStyle } from 'styled-components';
+
+ export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  html,
+  body,
+  #root {
+    height: 100%;
+  }
+
+  body {
+    font: 14px 'Roboto';
+    background: #ecf1f8;
+    color: #333;
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button, a {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+ `;
